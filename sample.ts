39ec504afa9b2a8366c09021a3d9a24c2ae12583
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-09-19 20:47:36 +0900

    first commit

---
+++ sample.ts
@@ -0,0 +1,18 @@
+//any
+//void
+
+function func(a: number, b: boolean): number {
+  return a;
+}
+
+// array
+let aa: number[] = [10, 20];
+
+//object
+interface Hoge {
+  a: number;
+  b?: number | null;
+  //....
+}
+
+let v: Hoge = { a: 10 };
